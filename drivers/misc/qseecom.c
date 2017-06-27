c93e3c398dddffb8be36ec81cd2b22e4dc7e2595
tsvietOK <tsvetkov_vlad@ukr.net>
2017-06-27 20:12:33 +0300

    qseecom: Change format specifier %p to %pK
    Format specifier %p can leak kernel addresses while not valuing the
    kptr_restrict system settings. When kptr_restrict is set to (1), kernel
    pointers printed using the %pK format specifier will be replaced with
    0's.
    So that %pK will not leak kernel pointers to unprivileged users.
    So change the format specifier from %p to %pK.
    
    Debugging Note : &pK prints only Zeros as address. if you need actual
    address information, pls echo 0 to kptr_restrict.
    $ echo 0 > /proc/sys/kernel/kptr_restrict
    
    Bug: 31498159
    Change-Id: I0baf2be2d5a476e2e4267f20b99d0ddf5492469e
    Signed-off-by: Mallikarjuna Reddy Amireddy <mamire@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1242,6 +1242,8 @@ static int qseecom_send_cmd(struct qseecom_dev_handle *data, void __user *argp)
 	if (ret)
 		return ret;
 
+	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%pK\n",
+			req.resp_len, req.resp_buf);
 	return ret;
 }
 
@@ -1351,6 +1353,8 @@ static int qseecom_send_modfd_cmd(struct qseecom_dev_handle *data,
 	if (ret)
 		return ret;
 
+	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%pK\n",
+			req.resp_len, req.resp_buf);
 	return ret;
 }
 
@@ -1815,7 +1819,7 @@ int qseecom_send_command(struct qseecom_handle *handle, void *send_buf,
 	if (ret)
 		return ret;
 
-	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%p\n",
+	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%pK\n",
 			req.resp_len, req.resp_buf);
 	return ret;
 }
